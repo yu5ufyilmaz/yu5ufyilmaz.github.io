23527d8a1960f92ff130b869383e7f812e9594dd
Yusuf Yilmaz <95490228+yu5ufyilmaz@users.noreply.github.com>
2025-11-01 13:06:25 +0300

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/yu5ufyilmaz.github.io/",
+  base: "/",
   plugins: [react()],
 });
